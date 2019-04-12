07ce24453e0cc7c432801cd4b06669f0b86d260a
Krešimir Ćurić <kresimir.curic1@gmail.com>
2019-04-12 09:50:08 +0200

    Fix: Forgot Password
    Promakao nam je ovaj sitan bug. Sad je rješeno! :)
    Mi nemamo ActionBar u tom Activityu, a pozivali smo setTitle metodu ...

--- app/src/main/java/com/puppy/asilo/Controller/PasswordRecoveryActivity.java
+++ app/src/main/java/com/puppy/asilo/Controller/PasswordRecoveryActivity.java
@@ -25,7 +25,6 @@ public class PasswordRecoveryActivity extends AppCompatActivity implements Passw
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.password_recovery_activity);
-        getSupportActionBar().setTitle(R.string.forgotPassword);
 
         mPasswordRecoveryHelper = new PasswordRecoveryHelper(this);
 
